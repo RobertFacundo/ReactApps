ee3dea51a20fb63987088fa2dc4bb898fd85d453
Facundo Robert <robert.facundo@hotmail.com>
2024-06-13 12:28:22 -0300

    index

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from './assets/react.svg'
+
+import './App.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='header'>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
+      </div>
+      <div className='container'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App

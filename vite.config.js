0da93a3f8139e7f757147f19cb9ded36ade66215
Facundo Robert <robert.facundo@hotmail.com>
2024-06-29 18:45:03 -0300

    !

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import sassPlugin from 'vite-plugin-sass'; // Asegúrate de importar vite-plugin-sass
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     sassPlugin(), // Usa el plugin de Sass aquí
+//   ],
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sassPlugin from 'vite-plugin-sass'; // Asegúrate de importar vite-plugin-sass
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sassPlugin(), // Usa el plugin de Sass aquí
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
